653bc68dc80d02c1817fa43b973b4cec9b119e91
sagnik88 <sagnik88@gmail.com>
2015-07-26 19:37:57 -0400

    changes for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,41 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+
+cacheval <- NULL
+  
+  setvalue <- function(y)
+  {
+    #setting new value to the parent variable x using 
+    x <<- y
+    cacheval <<- NULL
+    
+  }
+  
+  getvalue <- function()
+  {
+   x 
+    
+  }
+  
+  
+  caching <- function(val)
+  {
+    
+    cacheval <<- val
+  }
+  
+  
+  getinverse <- function()
+  {
+    cacheval
+    
+  }
+  
+  
+  list(setvalue = setvalue, getvalue = getvalue, caching = caching, getinverse = getinverse)
+  
+
 }
 
 
@@ -12,4 +47,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		
+		  inverseval <- x$getinverse()
+  
+  if(!is.null(inverseval)) #checking if exists in cache
+  {
+    cache
+    
+  }
+  
+  else   #if not in cache
+  {
+    
+    value2 <- x$getvalue()
+    inverseval <- solve(value2) #doing the inverse of the matrix
+    x$caching(inverseval) #storing in cache
+    inverseval
+    
+  }
 }
